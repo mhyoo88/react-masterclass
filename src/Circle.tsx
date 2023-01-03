1f2b4301e975b4548d72564f1688b2decf5f540a
mhyoo88 <minheeyoo@DESKTOP-1A7VFOD>
2023-01-03 17:08:15 +0900

    typescript - ?->optional prop, ??->default, es6 argument =  defualt

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -9,15 +10,24 @@ const Container = styled.div<ContainerProps>`
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string; //optional === borderColor: string | undefined;
+
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  //text = "" -> ES6 default 값
   //component의 prop에 전근 하는 방법. 인자로 받아온다.
-  return <Container bgColor={bgColor} />; //TypeScript가 봣을땐 Container 이기 때문에 bgColor라는 porp을 못받아온다. ContainerProps 을 추가 해주고 .div<ContainerProps> 추가 하면 에러가 사라진다.
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  ); // CircleProps 에선 borderColor가 옵셔널이라 Circle 컴포넌트에 borderColor가 없어도 에러가 안나지만 Container는 필수 값이라 에러가 난다 그렇다면 대체 할 default 값 보내기 할 수 있다.
 }
 
 export default Circle;
