b67a6aa26425dbfd96cc009800b59c07dc502b60
mhyoo88 <minheeyoo@DESKTOP-1A7VFOD>
2023-01-04 17:24:38 +0900

    useNavigate - 유저를 다른 페이지로 보낼때

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+  const onAboutClick = () => {
+    navigate("/about"); //유저가 로그인 했는데 redirect 하거나 유저가 어떤 페이지로 갔는데 접근 권한이 없을떄
+  };
   return (
     <header>
       <ul>
@@ -8,7 +12,8 @@ function Header() {
           <Link to={"/"}>Home</Link>
         </li>
         <li>
-          <Link to={"/about"}>About</Link>
+          {/* <Link to={"/about"}>About</Link> */}
+          <button onClick={onAboutClick}>About</button>
         </li>
       </ul>
     </header>
