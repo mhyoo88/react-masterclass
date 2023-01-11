244bbd1a9db3ea3de20e59d8a847bc94850aa8ad
mhyoo88 <minheeyoo@DESKTOP-1A7VFOD>
2023-01-11 15:34:59 +0900

    recoil - selector 사용하기 기본 1

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,5 @@
 import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import CreacteToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 import styled from "styled-components";
@@ -19,17 +19,37 @@ const TodoListUl = styled.ul`
 `;
 
 function TodoList() {
-  const toDos = useRecoilValue(toDoState); // value와  modifier 함수를 반환.
-  // console.log(toDos);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
+
   return (
     <div>
       <h1>Todos</h1>
-      <br />
+
+      <hr />
       <CreacteToDo />
+      <h2>To Do</h2>
       <TodoListUl>
         {/* {toDos.map((toDo) => <ToDo text={toDo.category} category={toDo.category} id={toDo.id}/>)} */}
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} /> //toDos 배열의 toDo 원소 하나하나가 ToDo 컴포넌트에 필요한 props 와 같은 모양이기 떄문에
+        ))}
+      </TodoListUl>
+
+      <hr />
+      <CreacteToDo />
+      <h2>Doing</h2>
+      <TodoListUl>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </TodoListUl>
+
+      <hr />
+      <CreacteToDo />
+      <h2>Done</h2>
+      <TodoListUl>
+        {done.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
         ))}
       </TodoListUl>
     </div>
