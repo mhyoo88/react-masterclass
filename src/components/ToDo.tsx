2611b5b9196242d3aff1fbfcfc1f5d2cfbc6fc6b
mhyoo88 <minheeyoo@DESKTOP-1A7VFOD>
2023-01-11 14:37:53 +0900

    toDos 버튼 구현 완료

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSetRecoilState } from "recoil";
+import { idText } from "typescript";
 import { IToDo, toDoState } from "../atoms";
 
 function ToDO({ text, category, id }: IToDo) {
@@ -8,7 +9,19 @@ function ToDO({ text, category, id }: IToDo) {
     const {
       currentTarget: { name },
     } = event;
-    console.log(`i wanna go to ${name}`);
+    setToDos((oldToDos) => {
+      console.log(oldToDos);
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldTodo = oldToDos[targetIndex];
+      const newTodo = { text, id, category: name as any };
+      console.log(oldTodo, newTodo); //{text: '3', id: 1673413782729, category: 'TO_DO'} {text: '3', id: 1673413782729, category: 'DOING'}
+      return [
+        //이후에 타입스크립트 에러가 나는데 oldToDos의 형태와 우리가 바꾸려는 newTodo의 category 가 달라서 에러가 난다 그러니까 category:name as any 를 추가해서 타입스크립트에 체크하지말라고 한다. name="" 으로 보내기 보다는 onCLick 함수에 인자를 보내는것으로 하는게 낫다. as any 를 쓰지 안하도 된다.
+        ...oldToDos.slice(0, targetIndex),
+        newTodo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
   };
   return (
     <li>
