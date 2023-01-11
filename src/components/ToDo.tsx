9b092ecaac214a594b41ca703428ca808d2c55a2
mhyoo88 <minheeyoo@DESKTOP-1A7VFOD>
2023-01-11 12:05:08 +0900

    각버튼에 이벤트 주기 - button 에 name을 이벤트에 전달해서 이벤트함수에서 처리하기

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,21 +1,32 @@
-import { IToDo } from "../atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDO({ text, category }: IToDo) {
-  const onClick = (newCategory: IToDo["category"]) => {
-    // "TO_DO" | "DOING" | "DONE" 할 수 있지만 이렇게 하면 보호 받으며 반복 하지않아도 된다
-    console.log(`i wanna go to ${newCategory}`);
+function ToDO({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    console.log(`i wanna go to ${name}`);
   };
   return (
     <li>
       <span>{text}</span>
       {category !== "DOING" && (
-        <button onClick={() => onClick("DOING")}>Doing</button>
+        <button name={"DOING"} onClick={onClick}>
+          Doing
+        </button>
       )}
       {category !== "TO_DO" && (
-        <button onClick={() => onClick("TO_DO")}>To Do</button>
+        <button name={"TO_DO"} onClick={onClick}>
+          To Do
+        </button>
       )}
       {category !== "DONE" && (
-        <button onClick={() => onClick("DONE")}>Done</button>
+        <button name={"DONE"} onClick={onClick}>
+          Done
+        </button>
       )}
     </li>
   );
